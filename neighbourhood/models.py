6a81f1edb85f52c779077f1b3023148d8889246f
Awadh-Awadh <awadh.awadhawadh.awadh@student.moringaschool.com>
2021-09-26 13:25:43 +0300

    add models and methods

--- neighbourhood/models.py
+++ neighbourhood/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.conf import settings
 from cloudinary.models import CloudinaryField
+from django.db.models.fields import CharField, EmailField
 # Create your models here.
 # user
 #neighbourhood
@@ -16,10 +17,11 @@ class CustomUser(AbstractUser):
         return self.username
 
 class Profile(models.Model):
-    name = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
     email = models.CharField(max_length=200)
     bio = models.TextField()
     hood = models.ForeignKey("Neighbourhood", on_delete=models.CASCADE)
+    pp = CloudinaryField('image')
 
 locations = [
     ('juja','juja'),
@@ -36,4 +38,46 @@ class Neighbourhood(models.Model):
     admin = models.ForeignKey(Profile, on_delete=models.CASCADE)
     police = models.IntegerField(blank=True,null= True)
     health = models.IntegerField(blank=True, null = True )
-    
+
+    class Meta:
+        ordering  = ['-pk']
+
+    def create_neighbourhood(self):
+        self.save()
+
+    def delete_neighbourhood(self):
+        self.delete()
+    @classmethod
+    def find_neighbourhood(cls, id):
+        return cls.objects.get(id = id)
+    @classmethod
+    def update_hood(cls, id, update_des):
+        updated_hood = cls.objects.filter(id = id).update(description = update_des)
+        return updated_hood
+        
+class Business(models.Model):
+    name = models.CharField(max_length=200)
+    bist_image = CloudinaryField('bist_image')
+    email = models,EmailField(max_length=100, blank = True)
+    mobile = models.CharField(max_length=15,blank = True)
+    hood = models.ForeignKey(Neighbourhood, on_delete = models.CASCADE)
+    personel = models.ForeignKey(Profile, on_delete=models.CASCADE)
+
+    def create_bist(self):
+        self.save()
+
+    def delete_bist(self):
+        self.delete()
+
+    @classmethod   
+    def search_bist(cls, bist_name):
+        return cls.objects.filter(name__icontains = bist_name).all()
+
+    def update_bist(cls,bist_id, update_des):
+        bist = cls.objects.filter(id =bist_id ).update(description = update_des )
+
+
+
+
+    def __str__(self):
+       return self.name
