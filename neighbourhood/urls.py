04481d8de7b5af57605ac0309d007f5e9cb6841e
Awadh-Awadh <awadh.awadhawadh.awadh@student.moringaschool.com>
2021-09-27 21:30:54 +0300

    add styling authentication temlates

--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth import views
-from .views import hero,home,register,profile, bist,post,contacts
+from .views import hero,home,register,profile, bist,post,contacts, search
 
 
 urlpatterns = [
@@ -12,6 +12,7 @@ urlpatterns = [
   path('profile/',profile, name = 'profile'),
   path('businesses/', bist, name= 'business' ),
   path('post/',post,name = 'post'),
-  path('contact/',contacts, name = 'contact')
+  path('contact/',contacts, name = 'contact'),
+  path('search-results/', search, name = 'search')
 
 ]
